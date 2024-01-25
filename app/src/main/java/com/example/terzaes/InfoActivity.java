d5509ba757c4ca146d463eb79d74597bf01d8c66
alvise <129381047+PrendinAlvise@users.noreply.github.com>
2024-01-25 11:06:20 +0100

    Update InfoActivity.java

--- app/src/main/java/com/example/terzaes/InfoActivity.java
+++ app/src/main/java/com/example/terzaes/InfoActivity.java
@@ -7,12 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class InfoActivity extends AppCompatActivity {
 
-    
+    //torno indietro alla pagina della calcolatrice 
     public void navigateToMainActivity(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
-
+    
+//creo un altra activity dove inserico le info dell'applicazione
+    
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
